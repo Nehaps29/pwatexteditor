be7d49a538b416cf5718dc69d2695f2301bbab34
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-11-11 13:36:51 -0500

    fixed put method in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Adding data');
   const jateDB = await openDB('jate',1);
@@ -29,7 +29,7 @@ export const putDb = async (content) => {
   }
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// gets all the content from the database
 export const getDb = async () =>  {
   console.log('Getting data');
   const jateDB = await openDB('jate',1);
